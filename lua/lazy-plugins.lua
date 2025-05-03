f2fd6dc2c5ac5fb724748b18b47f38d678dfb490
Kyle Krull <kdkrull@gmail.com>
2025-05-03 00:51:47 -0500

    Sort and organize

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -46,47 +46,34 @@
 -- you do for a plugin at the top level, you can do for a dependency.
 --
 -- Use the `dependencies` key to specify the dependencies of a particular plugin
+--
+-- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
+-- Or use telescope!
+-- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
+-- you can continue same window with `<space>sr` which resumes last telescope search
 require('lazy').setup({
-  require 'plugins.vim-sleuth',
-  require 'plugins.gitsigns-basic',
-  require 'plugins.which-key',
-  require 'plugins.telescope',
-  require 'plugins.lsp',
-  require 'plugins.conform',
-  require 'plugins.blink',
-
-  require 'plugins.colorscheme.kanagawa',
-
-  require 'plugins.todo-comments',
-  require 'plugins.mini',
-  require 'plugins.nvim-treesitter',
-
-  -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- place them in the correct locations.
-
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
+  -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.debug',
+  -- require 'kickstart.plugins.gitsigns',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'plugins.blink',
+  require 'plugins.colorscheme.kanagawa',
+  require 'plugins.conform',
+  require 'plugins.gitsigns-basic',
+  require 'plugins.lsp',
+  require 'plugins.mini',
+  require 'plugins.nvim-treesitter',
+  require 'plugins.telescope',
+  require 'plugins.todo-comments',
+  require 'plugins.vim-sleuth',
+  require 'plugins.which-key',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   -- { import = 'custom.plugins' },
-  --
-  -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
-  -- Or use telescope!
-  -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
-  -- you can continue same window with `<space>sr` which resumes last telescope search
 }, {
   rocks = {
     enabled = false,
