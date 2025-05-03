e86793a4fd0c25030c0fd364a76ba97c0052649a
Kyle Krull <kdkrull@gmail.com>
2025-05-03 01:02:04 -0500

    Organize

--- init.lua
+++ init.lua
@@ -27,19 +27,12 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
--- [[ Setting options ]]
+require 'config.autocommands'
+require 'config.keymap'
 require 'config.options'
 
--- [[ Basic Keymaps ]]
-require 'config.keymap'
-
--- [[ Basic Autocommands ]]
-require 'config.autocommands'
-
--- [[ Install `lazy.nvim` plugin manager ]]
+-- Install `lazy.nvim` plugin manager, then configure plugins
 require 'lazy-bootstrap'
-
--- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
