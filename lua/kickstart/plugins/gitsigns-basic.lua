71bb434b4c0eede3ffc6a0b56fa7eeebc4849427
Kyle Krull <kdkrull@gmail.com>
2025-05-02 23:45:56 -0500

    Extract another plugin spec

--- lua/kickstart/plugins/gitsigns-basic.lua
+++ lua/kickstart/plugins/gitsigns-basic.lua
@@ -1,7 +1,8 @@
 -- gitsigns basic configuration
+-- Adds git related signs to the gutter, as well as utilities for managing changes
 -- See `:help gitsigns` to understand what the configuration keys do
 return {
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+  {
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
