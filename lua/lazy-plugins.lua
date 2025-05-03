70fea00d83fc16f8169c5f90703f5966ea9e3d17
Kyle Krull <kdkrull@gmail.com>
2025-05-02 23:40:43 -0500

    Extract spec for one plugin

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -27,23 +27,7 @@ require('lazy').setup({
   --            })
   --        end,
   --    }
-  --
-  -- Here is a more advanced example where we pass configuration
-  -- options to `gitsigns.nvim`.
-  --
-  -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
+  require 'kickstart.plugins.gitsigns-basic',
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
@@ -746,7 +730,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  { import = 'custom.plugins' },
+  -- { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
